244a4b5b1499aca5c36de2c6c1f8bd202de42f4b
Grzegorz Adam Hankiewicz <gradha@imap.cc>
2014-07-17 16:33:18 +0200

    Adds compatibility for Nimrod's 0.9.4 compiler.

---
+++ src/babelpkg/compat.nim
@@ -0,0 +1,28 @@
+# Copyright (C) Dominik Picheta. All rights reserved.
+# BSD License. Look at license.txt for more info.
+
+## This module contains additional code from the development version of
+## Nimrod's standard library. These procs are required to be able to compile
+## against the last stable release 0.9.4. Once 0.9.6 is release these procs
+## will disappear.
+
+import json
+
+when not defined(`json.{}`):
+  proc `{}`*(node: PJsonNode, key: string): PJsonNode =
+    ## Transverses the node and gets the given value. If any of the
+    ## names does not exist, returns nil
+    result = node
+    if isNil(node): return nil
+    result = result[key]
+
+when not defined(`json.{}=`):
+  proc `{}=`*(node: PJsonNode, names: varargs[string], value: PJsonNode) =
+    ## Transverses the node and tries to set the value at the given location
+    ## to `value` If any of the names are missing, they are added
+    var node = node
+    for i in 0..(names.len-2):
+      if isNil(node[names[i]]):
+        node[names[i]] = newJObject()
+      node = node[names[i]]
+    node[names[names.len-1]] = value
