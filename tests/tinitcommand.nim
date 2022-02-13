cdd18d529c1ac74f84675a57ac8864b2a48d11f6
Locria Cyber <74560659+locriacyber@users.noreply.github.com>
2022-02-13 12:04:28 +0100

    Add test case for #983

---
+++ tests/tinitcommand.nim
@@ -0,0 +1,17 @@
+import unittest, os
+import testscommon
+
+from nimblepkg/common import cd
+
+suite "init":
+  ## https://github.com/nim-lang/nimble/pull/983
+  test "init within directory that is invalid package name will not create new directory":
+    cleanDir(installDir)
+    let tempdir = installDir / "a-b"
+    createDir(tempdir)
+    cd(tempdir):
+      let args = ["init"]
+      let (output, exitCode) = execNimbleYes(args)
+      check exitCode == QuitSuccess
+      check not dirExists("a_b")
+      check fileExists("a_b.nimble")
