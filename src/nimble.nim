adbf0770e8ea9ef1c8e3650c15ddba91a30e13cf
Ivan Yonchovski <yyoncho@gmail.com>
2022-09-01 13:42:01 +0300

    Move deps related stuff into separate package

--- src/nimble.nim
+++ src/nimble.nim
@@ -19,7 +19,8 @@ import nimblepkg/packageinfotypes, nimblepkg/packageinfo, nimblepkg/version,
        nimblepkg/checksums, nimblepkg/topologicalsort, nimblepkg/lockfile,
        nimblepkg/nimscriptwrapper, nimblepkg/developfile, nimblepkg/paths,
        nimblepkg/nimbledatafile, nimblepkg/packagemetadatafile,
-       nimblepkg/displaymessages, nimblepkg/sha1hashes, nimblepkg/syncfile
+       nimblepkg/displaymessages, nimblepkg/sha1hashes, nimblepkg/syncfile,
+       nimblepkg/deps
 
 const
   nimblePathsFileName* = "nimble.paths"
@@ -1630,52 +1631,6 @@ proc lock(options: Options) =
   updateSyncFile(pkgInfo, options)
   displayLockOperationFinish(doesLockFileExist)
 
-type
-  DependencyNode = ref object of RootObj
-    name*: string
-    version*: string
-    resolvedTo*: string
-    error*: string
-    dependencies*: seq[DependencyNode]
-
-proc depsRecursive(pkgInfo: PackageInfo,
-                   dependencies: seq[PackageInfo],
-                   errors: ValidationErrors): seq[DependencyNode] =
-  result = @[]
-  for (name, ver) in pkgInfo.requires:
-    var depPkgInfo = initPackageInfo()
-    let
-      found = dependencies.findPkg((name, ver), depPkgInfo)
-      packageName = if found: depPkgInfo.basicInfo.name else: name
-
-    let node = DependencyNode(name: packageName)
-
-    result.add node
-    node.version = if ver.kind == verAny: "@any" else: $ver
-    node.resolvedTo = if found: $depPkgInfo.basicInfo.version else: ""
-    node.error = if errors.contains(packageName):
-      getValidationErrorMessage(packageName, errors.getOrDefault packageName)
-    else: ""
-
-    if found:
-      node.dependencies = depsRecursive(depPkgInfo, dependencies, errors)
-
-proc printDepsHumanReadable(pkgInfo: PackageInfo, dependencies: seq[PackageInfo], level: int, errors: ValidationErrors) =
-  for (name, ver) in pkgInfo.requires:
-    var depPkgInfo = initPackageInfo()
-    let
-      found = dependencies.findPkg((name, ver), depPkgInfo)
-      packageName = if found: depPkgInfo.basicInfo.name else: name
-
-    echo " ".repeat(level * 2),
-      packageName,
-      if ver.kind == verAny: "@any" else: " " & $ver,
-      if found: fmt "(resolved {depPkgInfo.basicInfo.version})" else: "",
-      if errors.contains(packageName):
-        " - error: " & getValidationErrorMessage(packageName, errors.getOrDefault packageName)
-      else:
-        ""
-    if found: printDepsHumanReadable(depPkgInfo, dependencies, level + 1, errors)
 
 proc depsTree(options: Options) =
   ## Prints the dependency tree
