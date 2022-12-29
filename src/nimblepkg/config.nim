83324f7f761aaff17b4e21db03303eee2c1df8b8
xilicode <90548933+xilicode@users.noreply.github.com>
2022-12-29 17:12:29 +0800

    Update config.nim
    fix packageLists

--- src/nimblepkg/config.nim
+++ src/nimblepkg/config.nim
@@ -24,7 +24,7 @@ proc initConfig(): Config =
   result.cloneUsingHttps = true
   result.packageLists["official"] = PackageList(name: "Official", urls: @[
     "https://github.com/nim-lang/packages/raw/master/packages.json",
-    "https://irclogs.nim-lang.org/packages.json",
+    "https://raw.githubusercontent.com/nim-lang/packages/master/packages.json",
     "https://nim-lang.org/nimble/packages.json"
   ])
 
