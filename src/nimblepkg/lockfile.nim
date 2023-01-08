7313d6602017e3f295075d8b5c03952b96e6b607
Andreas Rumpf <rumpf_a@web.de>
2023-01-08 17:39:27 +0100

    Update src/nimblepkg/lockfile.nim

--- src/nimblepkg/lockfile.nim
+++ src/nimblepkg/lockfile.nim
@@ -36,11 +36,9 @@ proc writeLockFile*(fileName: string, packages: LockFileDeps,
       $lfjkPackages: packagesJsonNode
       }
 
-  let contents = block:
-    var s = mainJsonNode.pretty
-    s.add '\n'
-    s
-  writeFile(fileName, contents)
+  var s = mainJsonNode.pretty
+  s.add '\n'
+  writeFile(fileName, s)
 
 proc readLockFile*(filePath: string): LockFileDeps =
   {.warning[UnsafeDefault]: off.}
