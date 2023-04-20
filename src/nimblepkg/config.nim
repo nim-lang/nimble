4345542517c3fa007c0fcfc459ac2bad878c352a
xilicode <90548933+xilicode@users.noreply.github.com>
2023-04-21 00:09:43 +0800

    replaces the redirect link with the direct link (#1054)
    * Update config.nim
    
    fix packageLists
    
    * Replacing the redirect link with the direct link in order to save one redirect, which is quite important for users with poor/semi-broken internet.
    
    ---------
    
    Co-authored-by: ringabout <43030857+ringabout@users.noreply.github.com>

--- src/nimblepkg/config.nim
+++ src/nimblepkg/config.nim
@@ -23,7 +23,7 @@ proc initConfig(): Config =
   result.chcp = true
   result.cloneUsingHttps = true
   result.packageLists["official"] = PackageList(name: "Official", urls: @[
-    "https://github.com/nim-lang/packages/raw/master/packages.json",
+    "https://raw.githubusercontent.com/nim-lang/packages/master/packages.json",
     "https://nim-lang.org/nimble/packages.json"
   ])
 
