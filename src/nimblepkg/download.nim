daef32b4d04e0b532ec9e3535eee937626bf2bcf
Bung <crc32@qq.com>
2023-05-22 08:20:55 +0800

    avoid re-download

--- src/nimblepkg/download.nim
+++ src/nimblepkg/download.nim
@@ -496,7 +496,7 @@ proc downloadPkg*(url: string, verRange: VersionRange,
   if verRange.kind != verSpecial:
     ## Makes sure that the downloaded package's version satisfies the requested
     ## version range.
-    let pkginfo = getPkgInfo(result[0], options)
+    let pkginfo = getPkgInfo(result.dir, options)
     if pkginfo.basicInfo.version notin verRange:
       raise nimbleError(
         "Downloaded package's version does not satisfy requested version " &
