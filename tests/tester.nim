4ed0fe26c10a3c5ebe2500463ac6856ea1864163
jmgomez <info@jmgomez.me>
2024-02-17 20:11:41 +0000

    re-enable tests

--- tests/tester.nim
+++ tests/tester.nim
@@ -1,33 +1,33 @@
 # Copyright (C) Dominik Picheta. All rights reserved.
 # BSD License. Look at license.txt for more info.
 
-# import testscommon
+import testscommon
 
 # suits imports
 
-# import tinitcommand
-# import tcheckcommand
-# import tcleancommand
-# import tdevelopfeature
-# import tissues
-# import tlocaldeps
-# import tlockfile
-# import tmisctests
-# import tmoduletests
-# import tmultipkgs
-# import tnimbledump
-# import tnimblerefresh
-# import tnimbletasks
-# import tnimscript
-# import tshellenv
-# import tpathcommand
-# import treversedeps
-# import truncommand
+import tinitcommand
+import tcheckcommand
+import tcleancommand
+import tdevelopfeature
+import tissues
+import tlocaldeps
+import tlockfile
+import tmisctests
+import tmoduletests
+import tmultipkgs
+import tnimbledump
+import tnimblerefresh
+import tnimbletasks
+import tnimscript
+import tshellenv
+import tpathcommand
+import treversedeps
+import truncommand
 import tsetupcommand
-# import ttestcommand
-# import ttwobinaryversions
-# import tuninstall
-# import ttaskdeps
+import ttestcommand
+import ttwobinaryversions
+import tuninstall
+import ttaskdeps
 
 # nonim tests are very slow and (often) break the CI.
 
