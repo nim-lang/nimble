876fb4157114338dbb94b9b33a18461d4dadea0f
Juan M Gómez <juamangomalv@gmail.com>
2024-03-03 16:49:50 +0000

    Warn when packages.json doesnt exists rather than crash (#1186)

--- src/nimblepkg/packageinfo.nim
+++ src/nimblepkg/packageinfo.nim
@@ -114,6 +114,7 @@ proc validatePackagesList(path: string): bool =
   ## Determines whether package list at ``path`` is valid.
   try:
     if not path.fileExists:
+      display("Warning:", path & " does not exist.", Warning, HighPriority)
       return false
     let pkgList = parseFile(path)
     if pkgList.kind == JArray:
