b264c14b017c73d8e71ef58036591c837f31b84b
xTrayambak <xtrayambak@disroot.org>
2024-05-27 10:05:52 +0530

    (rm) remove unused import

--- src/nimblepkg/forge_aliases.nim
+++ src/nimblepkg/forge_aliases.nim
@@ -1,7 +1,7 @@
 # Copyright (C) Dominik Picheta. All rights reserved.
 # BSD License. Look at license.txt for more info.
 
-import std/[strutils, uri]
+import std/[strutils]
 import common, download
 
 type
