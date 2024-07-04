fa09e487bdb5541dc205043fe00828de4e1acc79
Juan M Gómez <info@jmgomez.me>
2024-07-04 19:59:19 +0100

    Improves nim installation by using csources (same as atlas) (#1233)
    * draft: improves nim installation by using csources (same as atlas)
    
    Notice when SAT is enabled, csources is stored in the cache.
    Missing:
    -- Proper reporting
    -- Handle special versions (assume csources_2?). We could have special cases for #version-x
    - Refactor to another file (nimble.nim is crazy big already)
    
    * progress
    
    * refactor: extract file nimenv
    
    * Fixes an issue where the packageCache wasnt being shared in local mode
    
    * [Green]Should be able to install different Nim versions
    
    * fix warnings
    
    * missing display
    
    * fix typo
    
    * Remove nimble from nim compilation Fixes #1175

---
+++ src/nimblepkg/nimenv.nim
@@ -0,0 +1,110 @@
+import std/[strscans, os, strutils, strformat]
+import version, nimblesat, cli, common, options
+
+when defined(windows):
+  const
+    BatchFile = """
+  @echo off
+  set PATH="$1";%PATH%
+  """
+else:
+  const
+    ShellFile = "export PATH=$1:$$PATH\n"
+
+const ActivationFile = 
+  when defined(windows): "activate.bat" else: "activate.sh"
+
+proc infoAboutActivation(nimDest, nimVersion: string) =
+  when defined(windows):
+    display("Info", nimDest & "installed; activate with 'nim-" & nimVersion & "activate.bat'")
+  else:
+    display("Info", nimDest & "installed; activate with 'source nim-" & nimVersion & "activate.sh'")
+
+proc compileNim*(options: Options, nimDest: string, v: VersionRange) =
+  let keepCsources = options.useSatSolver #SAT Solver has a cache instead of a temp dir for downloads
+  template exec(command: string) =
+    let cmd = command # eval once
+    if os.execShellCmd(cmd) != 0:
+      display("Error", "Failed to execute: $1" % cmd, Error, HighPriority)
+      return
+  let nimVersion = v.getNimVersion()
+  let workspace = nimDest.parentDir()
+  if dirExists(workspace / nimDest):
+    if not fileExists(nimDest / ActivationFile):
+      display("Info", &"Directory {nimDest} already exists; remove or rename and try again")
+    else:
+      infoAboutActivation nimDest, $nimVersion
+    return
+
+  var major, minor, patch: int
+  if not nimVersion.isSpecial: 
+    if not scanf($nimVersion, "$i.$i.$i", major, minor, patch):
+      display("Error", "cannot parse version requirement", Error)
+      return
+  let csourcesVersion =
+    #TODO We could test special against the special versionn-x branch to get the right csources
+    if nimVersion.isSpecial or (major == 1 and minor >= 9) or major >= 2:
+      # already uses csources_v2
+      "csources_v2"
+    elif major == 0:
+      "csources" # has some chance of working
+    else:
+      "csources_v1"
+  cd workspace:
+    echo "Entering CSOURCES", csourcesVersion, " exists ", dirExists(csourcesVersion)
+    if not dirExists(csourcesVersion):
+      exec "git clone https://github.com/nim-lang/" & csourcesVersion
+
+  cd workspace / csourcesVersion:
+    when defined(windows):
+      exec "build.bat"
+    else:
+      let makeExe = findExe("make")
+      if makeExe.len == 0:
+        exec "sh build.sh"
+      else:
+        exec "make"
+  let nimExe0 = ".." / csourcesVersion / "bin" / "nim".addFileExt(ExeExt)
+  cd nimDest:
+    let nimExe = "bin" / "nim".addFileExt(ExeExt)
+    copyFileWithPermissions nimExe0, nimExe
+    exec nimExe & " c --noNimblePath --skipUserCfg --skipParentCfg --hints:off koch"
+    let kochExe = when defined(windows): "koch.exe" else: "./koch"
+    exec kochExe & " boot -d:release --skipUserCfg --skipParentCfg --hints:off"
+    exec kochExe & " tools --skipUserCfg --skipParentCfg --hints:off"
+    # unless --keep is used delete the csources because it takes up about 2GB and
+    # is not necessary afterwards:
+    if not keepCsources:
+      removeDir workspace / csourcesVersion / "c_code"
+    let pathEntry = workspace / nimDest / "bin"
+    #remove nimble so it doesnt interfer with the current one:
+    removeFile "bin" / "nimble".addFileExt(ExeExt)
+    when defined(windows):
+      writeFile "activate.bat", BatchFile % pathEntry.replace('/', '\\')
+    else:
+      writeFile "activate.sh", ShellFile % pathEntry
+    infoAboutActivation nimDest, $nimVersion
+
+
+proc useNimFromDir*(options: var Options, realDir: string, v: VersionRange, tryCompiling = false) =
+  const binaryName = when defined(windows): "nim.exe" else: "nim"
+
+  let
+    nim = realDir / "bin" / binaryName
+    fileExists = fileExists(options.nimBin)
+
+  if not fileExists(nim):
+    if tryCompiling and options.prompt("Develop version of nim was found but it is not compiled. Compile it now?"):
+      compileNim(options, realDir, v)
+    else:
+      raise nimbleError("Trying to use nim from $1 " % realDir,
+                        "If you are using develop mode nim make sure to compile it.")
+
+  options.nimBin = nim
+  let separator = when defined(windows): ";" else: ":"
+
+  putEnv("PATH", realDir / "bin" & separator & getEnv("PATH"))
+  if fileExists:
+    display("Info:", "switching to $1 for compilation" % options.nim, priority = HighPriority)
+  else:
+    display("Info:", "using $1 for compilation" % options.nim, priority = HighPriority)
---
+++ tests/tniminstall.nim
@@ -0,0 +1,32 @@
+
+{.used.}
+
+import unittest, os, strutils, sequtils, strscans
+import testscommon
+from nimblepkg/common import cd
+
+proc isNimPkgVer(folder: string, ver: string): bool = 
+  let name = folder.split("-")
+  result = name.len == 3 and name[1].contains(ver)
+  echo "Checking ", folder, " for ", ver, " result: ", result
+  if ver == "devel":
+  #We know devel is bigger than 2.1 and it should be an odd number (notice what we test here is actually the #)
+    var major, minor, patch: int 
+    if scanf(name[1], "$i.$i.$i", major, minor, patch):
+      return major >= 2 and minor >= 1 and minor mod 2 == 1
+    else: return false
+
+     
+  
+
+suite "Nim install":
+  test "Should be able to install different Nim versions":
+    cd "nimnimble":
+      for nimVerDir in ["nim1.6.20", "nim2.0.4", "nimdevel"]:
+        cd nimVerDir:
+          let nimVer = nimVerDir.replace("nim", "")
+          echo "Checking version ", nimVer
+          let (_, exitCode) = execNimble("install", "-l")
+          let pkgPath = getCurrentDir() / "nimbledeps" / "pkgs2"
+          check exitCode == QuitSuccess
+          check walkDir(pkgPath).toSeq.anyIt(it[1].isNimPkgVer(nimVer))      
