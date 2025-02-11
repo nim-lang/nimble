178e2825530311666e284dc094faed79be44cff7
jmgomez <info@jmgomez.me>
2025-02-11 09:55:17 +0000

    Fixes a crash in the tests (unrelated to the feature)

--- src/nimblepkg/downloadnim.nim
+++ src/nimblepkg/downloadnim.nim
@@ -123,10 +123,12 @@ proc getGccArch*(options: Options): int =
     return when defined(windows): 32 else: 64
 
 proc isRosetta*(): bool =
-  let res = gorgeEx("sysctl -in sysctl.proc_translated")
-  if res.exitCode == 0:
-    return res.output.strip() == "1"
-  return false
+  try:
+    let res = execCmdEx("sysctl -in sysctl.proc_translated")
+    if res.exitCode == 0:
+      return res.output.strip() == "1"
+  except CatchableError as e:
+    return false
 
 proc getNightliesUrl*(parsedContents: JsonNode, arch: int): (string, string) =
   let os =
@@ -181,7 +183,7 @@ proc doCmdRaw*(cmd: string) =
   stdout.flushFile()
   stderr.flushFile()
 
-  if defined(macosx):
+  if defined(macosx) and isRosetta():
     command = "arch -arm64 " & command
 
   displayDebug("Executing", command)
