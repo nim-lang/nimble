78eb27284b45e1dce0d31814b609568b2c800551
jmgomez <info@jmgomez.me>
2025-06-18 12:41:26 +0100

    Improves TNimInstall test

--- tests/tniminstall.nim
+++ tests/tniminstall.nim
@@ -4,17 +4,13 @@
 import unittest, os, strutils, sequtils, strscans
 import testscommon
 from nimblepkg/common import cd
+import nimblepkg/[declarativeparser, options, version]
+
 
 proc isNimPkgVer(folder: string, ver: string): bool = 
-  let name = folder.split("-")
-  result = name.len == 3 and name[1].contains(ver)
-  echo "Checking ", folder, " for ", ver, " result: ", result
-  if ver == "devel":
-  #We know devel is bigger than 2.1 and it should be an odd number (notice what we test here is actually the #)
-    var major, minor, patch: int 
-    if scanf(name[1], "$i.$i.$i", major, minor, patch):
-      return major >= 2 and minor >= 1 and minor mod 2 == 1
-    else: return false
+  let nimPkg = getPkgInfoFromDirWithDeclarativeParser(folder, initOptions())
+  echo "Checking ", folder, " for ", ver, " result: ", nimPkg.basicInfo.name, " ", $nimPkg.basicInfo.version
+  result = nimPkg.basicInfo.name == "nim" and nimPkg.basicInfo.version == newVersion(ver)
 
 
 suite "Nim install":
@@ -23,9 +19,7 @@ suite "Nim install":
       for nimVerDir in ["nim2.0.4"]:
         cd nimVerDir:
           let nimVer = nimVerDir.replace("nim", "")
-          echo "Checking version ", nimVer
           let (_, exitCode) = execNimble("install", "-l")
           let pkgPath = getCurrentDir() / "nimbledeps" / "pkgs2"
-          echo "Checking ", pkgPath
           check exitCode == QuitSuccess
           check walkDir(pkgPath).toSeq.anyIt(it[1].isNimPkgVer(nimVer))      
