ff5dbd3acb28b43fc73e74ef62f2f0c07028c5d0
jmgomez <info@jmgomez.me>
2025-07-01 10:53:01 +0100

    makes vnext respects the package file name for the module.

--- tests/develop/dependency/dependency.nim
+++ tests/develop/dependency/dependency.nim
@@ -2,7 +2,8 @@ import packagea
 
 proc test*(): string =
   when defined(windows) or defined(macosx):
-    $PackageA.test(6, 9)
+    $packagea.test(6, 9) #This will fail in the old code path as the babel name for packageA is CamelCase.
+    # $PackageA.test(6, 9) 
   elif defined(unix):
     $packagea.test(6, 9)
   else:
