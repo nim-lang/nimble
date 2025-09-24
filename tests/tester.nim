2ecfa95021a7e1c6d273e091d2031c69c69abe5e
jmgomez <info@jmgomez.me>
2025-09-24 10:10:48 +0100

    Disables all tests except the suite that fails only in the CI

--- tests/tester.nim
+++ tests/tester.nim
@@ -4,38 +4,38 @@
 import testscommon
 
 # suits imports
-import tnimbinaries
-import tinitcommand
-import tcheckcommand
-import tcleancommand
-import tdevelopfeature
-import tissues
-import tlocaldeps
-import tlockfile
-import tmisctests
-import tmoduletests
-import tmultipkgs
-import tnimbledump
-import tnimblerefresh
-import tnimbletasks
-import tnimscript
-import tshellenv
-import tpathcommand
-import treversedeps
-import truncommand
-import tsetupcommand
-import ttestcommand
-import ttwobinaryversions
-import tuninstall
+# import tnimbinaries
+# import tinitcommand
+# import tcheckcommand
+# import tcleancommand
+# import tdevelopfeature
+# import tissues
+# import tlocaldeps
+# import tlockfile
+# import tmisctests
+# import tmoduletests
+# import tmultipkgs
+# import tnimbledump
+# import tnimblerefresh
+# import tnimbletasks
+# import tnimscript
+# import tshellenv
+# import tpathcommand
+# import treversedeps
+# import truncommand
+# import tsetupcommand
+# import ttestcommand
+# import ttwobinaryversions
+# import tuninstall
 import ttaskdeps
-import tsat
-import tniminstall
-import trequireflag
-import tdeclarativeparser
-import tforgeinstall
-import tforgeparser
-import tfilepathrequires
-import tglobalinstall
+# import tsat
+# import tniminstall
+# import trequireflag
+# import tdeclarativeparser
+# import tforgeinstall
+# import tforgeparser
+# import tfilepathrequires
+# import tglobalinstall
 # # nonim tests are very slow and (often) break the CI.
 
 # # import tnonim
