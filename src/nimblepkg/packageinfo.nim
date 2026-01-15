f4448483212e47b6815c1a25afe5e4bde6d8ad06
Juan M Gómez <info@jmgomez.me>
2026-01-15 17:01:30 +0000

    Makes global the new default (next release only) (#1564)
    * Makes global the new default (next release only)
    
    * Fixes win ci
    
    * Skip tests dir on install
    
    * fix

--- src/nimblepkg/packageinfo.nim
+++ src/nimblepkg/packageinfo.nim
@@ -449,7 +449,7 @@ proc checkInstallDir(pkgInfo: PackageInfo,
   elif thisDir[0] == '.':
     result = true
 
-  if thisDir == "nimcache": result = true
+  if thisDir == "nimcache" or thisDir == "tests": result = true
 
 proc iterFilesWithExt(dir: string, pkgInfo: PackageInfo,
                       action: proc (f: string)) =
@@ -487,9 +487,9 @@ proc iterInstallFilesSimple*(realDir: string, pkgInfo: PackageInfo,
           skipDir = true
           break
 
-      # Skip nimcache
+      # Skip nimcache and tests directories
       let dirName = splitPath(file).tail
-      if dirName == "nimcache":
+      if dirName == "nimcache" or dirName == "tests":
         skipDir = true
 
       # Skip nested nimble directories
