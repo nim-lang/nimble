c555372cfcc3dbfae2444d9380471045d4e47af9
jmgomez <info@jmgomez.me>
2026-03-04 10:01:54 +0000

    Fix nimble shell skipping global pkgcache (fixes #1610)

--- tests/tlocaldeps.nim
+++ tests/tlocaldeps.nim
@@ -3,21 +3,44 @@
 
 {.used.}
 
-import unittest, os, osproc, strformat
+import unittest, os, osproc, strutils, strformat
 import testscommon
 from nimblepkg/common import cd
-import nimblepkg/options
+import nimblepkg/[options, config]
 
 suite "project local deps mode":
   setup:
-    # do this to prevent non-deterministic (random?) setting of the NIMBLE_DIR 
     # which messes up this test sometime
     delEnv("NIMBLE_DIR")
+
+  test "NIMBLE_DIR set to nimbledeps uses global pkgcache (issue #1610)":
+    ## When `nimble shell -l` runs, it sets NIMBLE_DIR to the local nimbledeps
+    ## directory. A nested nimble process picks up NIMBLE_DIR and should still
+    ## use the global pkgcache (~/.nimble/pkgcache), not nimbledeps/pkgcache.
+    cd "localdeps":
+      cleanDir("nimbledeps")
+      createDir("nimbledeps")
+      let nimbledepsAbs = expandFilename("nimbledeps")
+      # Simulate what `nimble shell` does: set NIMBLE_DIR to nimbledeps
+      putEnv("NIMBLE_DIR", nimbledepsAbs)
+      defer: delEnv("NIMBLE_DIR")
+
+      var options = initOptions()
+      options.config = parseConfig()
+      options.action = Action(typ: actionInstall)
+      options.setNimbleDir()
+
+      # nimbleDir should be the nimbledeps path (from NIMBLE_DIR env)
+      check options.nimbleDir == nimbledepsAbs
+      # pkgCachePath should use the GLOBAL config dir, not nimbledeps
+      let globalPkgCache = expandTilde(options.config.nimbleDir).absolutePath() / "pkgcache"
+      check options.pkgCachePath == globalPkgCache
+      check options.pkgCachePath.find("nimbledeps") == -1
 
   test "nimbledeps exists":
     cd "localdeps":
       removeFile("localdeps")
-      cleanDir("nimbledeps")
+
       # TEMPORARY: Added for global-by-default. To revert to local-by-default, remove this createDir line:
       createDir("nimbledeps")
       let (_, exitCode) = execCmdEx(nimblePath & " install -y")
