e10b0c799a17de20caec4e514802b11b4d8893b2
Daylin Morgan <me@dayl.in>
2026-03-29 18:14:44 -0400

    refactor tests for the serial run

--- tests/tskipbin.nim
+++ tests/tskipbin.nim
@@ -1,5 +1,4 @@
-# Tests that with --skipBin hybrid packages ares not compiled
-# packages are built in a temp directory and only necessary files are installed
+# Tests that with --skipBin hybrid packages are not compiled
 
 {.used.}
 
@@ -12,26 +11,33 @@ func exe(name: string): string =
   when defined(windows): name & ".exe" else: name
 
 template checkSkip(cmd: string, toSkip = false) =
-  cd "pkgWithHybridDep":
-    cleanDir installDir
-    cleanFiles nimblePathsFileName, nimbleConfigFileName
-    var args = @[cmd]
-    if toSkip: args.add "--skipBin"
-    let res = execNimbleYes(args)
-    verify res
-    let hybridPkgDir = getPackageDir(pkgsDir, "hybrid")
-    check hybridPkgDir.len > 0
-    check "Building hybrid/hybrid" in res.output != toSkip
-    check fileExists(hybridPkgDir / "hybrid".exe) != toSkip
-    check fileExists(installDir / "bin" / "hybrid") != toSkip
-    # --skipBin should not effect the root package
-    if cmd == "install":
-      check fileExists(installDir / "bin" / "pkgWithHybridDep")
+  var args = @[cmd]
+  if toSkip: args.add "--skipBin"
+  let res = execNimbleYes(args)
+  verify res
+  let hybridPkgDir = getPackageDir(pkgsDir, "hybrid")
+  check hybridPkgDir.len > 0
+  check "Building hybrid/hybrid" in res.output != toSkip
+  check fileExists(hybridPkgDir / "hybrid".exe) != toSkip
+  check fileExists(installDir / "bin" / "hybrid") != toSkip
+  # --skipBin should not effect the root package
+  if cmd == "install":
+    check fileExists(installDir / "bin" / "pkgWithHybridDep")
+
+template runTest(name: string,  body: untyped) =
+  test name:
+    cd "pkgWithHybridDep":
+      cleanDir installDir
+      cleanFiles nimblePathsFileName, nimbleConfigFileName
+      body
 
 suite "--skipBin":
   for command in ["setup", "install"]:
-    test command & " without --skipBin":
+    runTest command & " without --skipBin":
       checkSkip(command, false)
-    test command & " with --skipBin":
+    runTest command & " with --skipBin":
       checkSkip(command, true)
+    runTest command & " with --skipBin then without --skipBin":
+      checkSkip(command, true)
+      checkSkip(command, false)
 
