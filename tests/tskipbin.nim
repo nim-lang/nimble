7ae5a3aa8c055f1af4f38513c46a1ccc9d8a91f1
Daylin Morgan <me@dayl.in>
2026-03-31 16:09:13 -0400

    better suite name; drop templates from tskipbin

--- tests/tskipbin.nim
+++ tests/tskipbin.nim
@@ -7,33 +7,24 @@ import testscommon
 from nimble import nimblePathsFileName, nimbleConfigFileName
 from nimblepkg/common import cd
 
-func exe(name: string): string =
-  when defined(windows): name & ".exe" else: name
-
-template checkSkip(cmd: string, toSkip = false) =
-  var args = @[cmd]
-  if toSkip: args.add "--skipBin"
-  let res = execNimbleYes(args)
-  verify res
-  let hybridPkgDir = getPackageDir(pkgsDir, "hybrid")
-  check hybridPkgDir.len > 0
-  check "Building hybrid/hybrid" in res.output != toSkip
-  check fileExists(hybridPkgDir / "hybrid".exe) != toSkip
-  check fileExists(installDir / "bin" / "hybrid") != toSkip
-  # --skipBin should not effect the root package
-  if cmd == "install":
-    check fileExists(installDir / "bin" / "pkgWithHybridDep")
-
-template runTest(name: string,  body: untyped) =
-  test name:
-    cd "pkgWithHybridDep":
-      cleanDir installDir
-      cleanFiles nimblePathsFileName, nimbleConfigFileName
-      body
-
-suite "--skipBin":
-  for command in ["setup", "install"]:
-    runTest command & " with --skipBin & without --skipBin":
-      checkSkip(command, true)
-      checkSkip(command, false)
-
+suite "skip compilation of hybrid packages with --skipBin":
+  for cmd in ["setup", "install"]:
+    test cmd & " with --skipBin then without --skipBin":
+      cd "pkgWithHybridDep":
+        cleanDir installDir
+        cleanFiles nimblePathsFileName, nimbleConfigFileName
+        for toSkip in [true, false]:
+          var args = @[cmd]
+          if toSkip: args.add "--skipBin"
+          let res = execNimbleYes(args)
+          verify res
+          let hybridPkgDir = getPackageDir(pkgsDir, "hybrid")
+          check hybridPkgDir.len > 0
+          check "Building hybrid/hybrid" in res.output != toSkip
+          check fileExists(
+            hybridPkgDir / "hybrid" & (when defined(windows): ".exe" else: "")
+          ) != toSkip
+          check fileExists(installDir / "bin" / "hybrid") != toSkip
+          # --skipBin should not effect the root package
+          if cmd == "install":
+            check fileExists(installDir / "bin" / "pkgWithHybridDep")
