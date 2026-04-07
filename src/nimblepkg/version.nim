ade91074eac68d21859c122e027c2e8d5e0d767b
jmgomez <info@jmgomez.me>
2026-04-07 15:48:13 +0100

    move PkgTuple JSON hooks after $ VersionRange to avoid corrupted cache writes

--- src/nimblepkg/version.nim
+++ src/nimblepkg/version.nim
@@ -364,17 +364,6 @@ proc parseRequires*(req: string): PkgTuple =
   if result.name.isForgeAlias:
     result.name = newForge(result.name).expand()
 
-proc initFromJson*(dst: var PkgTuple, jsonNode: JsonNode, jsonPath: var string) =
-  dst = parseRequires(jsonNode.str)
-
-proc toJsonHook*(src: PkgTuple): JsonNode =
-  let ver = if src.ver.kind == verAny: "" else: $src.ver
-  case src.ver.kind
-  of verAny: newJString(src.name)
-  of verSpecial: newJString(src.name & ver)
-  else:
-    newJString(src.name & " " & ver)
-
 proc `$`*(verRange: VersionRange): string =
   case verRange.kind
   of verLater:
@@ -399,6 +388,17 @@ proc `$`*(verRange: VersionRange): string =
     return "any version"
 
   result.add($verRange.ver)
+
+proc initFromJson*(dst: var PkgTuple, jsonNode: JsonNode, jsonPath: var string) =
+  dst = parseRequires(jsonNode.str)
+
+proc toJsonHook*(src: PkgTuple): JsonNode =
+  let ver = if src.ver.kind == verAny: "" else: $src.ver
+  case src.ver.kind
+  of verAny: newJString(src.name)
+  of verSpecial: newJString(src.name & ver)
+  else:
+    newJString(src.name & " " & ver)
 
 proc getSimpleString*(verRange: VersionRange): string =
   ## Gets a string with no special symbols and spaces. Used for dir name
