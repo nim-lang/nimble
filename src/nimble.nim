2a23502d80f5c98dafede6ace7a8ed8c5f0a34a1
jmgomez <info@jmgomez.me>
2026-08-03 16:09:29 +0100

    Lock implies setup. Fixes #179

--- src/nimble.nim
+++ src/nimble.nim
@@ -2444,6 +2444,7 @@ proc doAction(options: var Options, nimBinParam: Option[string]) {.instrument.} 
     check(options, nimBin)
   of actionLock:
     lock(options, nimBin)
+    setup(options, nimBin)
   of actionDeps:
     deps(options, nimBin)
   of actionSync:
